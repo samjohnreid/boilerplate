80776e9824a4fd2c3ff58b95e32b3ad359f60416
Samuel Reid <sam@Samuels-iMac.attlocal.net>
2020-05-24 19:00:49 -0400

    adding more products

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,15 @@ function getProducts() {
       <div class="items__grid">
         <ul class="items__list">
         ${
-          data.map((product, index) => {
+          data.map((product) => {
+            console.log(product.title.replace(/[^a-z0-9]/gi, '-').toLowerCase()); // TODO: remove for production
             return (`
-              <li class="items__item items__item--item-0${index + 1}"><a href="${product.url}" target="_blank"><span>${product.title}</span></a></li>
+              <li class="items__item">
+                <a href="${product.url}" target="_blank">
+                  <h2>${product.title}</h2>
+                  <img src="img/${product.img}" alt="Image for ${product.title}">
+                </a>
+              </li>
             `)
           }).join('')
         }
