3ff91e1a0e3c01b68cc751578c4eba3315d1c12c
Sam Reid <sreid@Sam-Reids-MBP.attlocal.net>
2021-11-15 12:38:12 -0500

    functionality for accordion

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,69 @@
 const accordionItems = document.querySelectorAll('.accordion__item');
 
-const toggleAccordion = (event) => {
-    const item = event.currentTarget;
+class Accordion {
+    constructor (item) {
+        this.item = item;
+        this.summary = item.children[0];
+        this.content = item.children[1];
 
-    const summary = item.children[0];
-    const content = item.children[1];
+        this.animation = null;
+        this.summary.addEventListener('click', (e) => this.onClick(e));
+    }
     
-    // Get start height of element
-    const startHeight = `${summary.offsetHeight}px`;
+    onClick(e) {
+        // hijack the default behavior of the accordion content expanding
+        e.preventDefault();
 
-    // Get the open height of the element
-    item.open = true;
-    const endHeight = `${content.offsetHeight}px`;
+        // when we later manually expand the content in order to retrieve it's height,
+        // first hide it until we later run the animation
+        this.item.style.overflow = 'hidden';
+        
+        this.item.open ? this.shrink() : this.open();
+    }
 
-    console.log('startHeight', startHeight);
-    console.log('endHeight', endHeight);
+    open() {
+        // add a fixed height that us the height of the summary button
+        this.item.style.height = `${this.summary.offsetHeight}px`;
 
-    if (item.open) {
-        console.log('item is open!');
-    } else {
-        console.log('item is closed!');
+        // add the [open] attribute to the accordion item
+        this.item.open = true;
+
+        // tell the animation to move on to the next frame and call the expand function
+        window.requestAnimationFrame(() => this.expand());
+    }
+
+    expand() {
+        const startHeight = `${this.item.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+
+        this.animation = this.item.animate(
+            { height: [startHeight, endHeight] },
+            { duration: 400, easing: 'ease-out' }
+        );
+
+        this.animation.onfinish = () => this.onAnimationFinish(true);
+    }
+
+    shrink() {
+        const startHeight = `${this.item.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight}px`;
+
+        this.animation = this.item.animate(
+            { height: [startHeight, endHeight] },
+            { duration: 400, easing: 'ease-out' }
+        );
+
+        this.animation.onfinish = () => this.onAnimationFinish(false);
+    }
+
+    onAnimationFinish(open) {
+        this.item.open = open;
+        this.animation = null;
+        this.item.style.height = '';
+        this.item.style.overflow = '';
     }
 }
 
-accordionItems.forEach((el) => {
-    el.addEventListener('click', toggleAccordion);
+accordionItems.forEach((item) => {
+    new Accordion(item);
 });
-
-
-// const startHeight = `${this.el.offsetHeight}px`;
